6c2c7aa717c8b02aee4019497f9faad82c67c30d
npham49 <brian.d.pham@gov.bc.ca>
2023-11-09 19:22:36 -0800

    added code for usestate and use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,54 @@
 import './App.css'
+// Step 1: import useState
+// Step 5: import useEffect
+import { useState, useEffect } from 'react'
 
 function App() {
-  // Step 3: create a function that chage the color of the header
-  const changeColor = () => {
-    const header = document.querySelector('.header-text')
-    header.style.color = 'white'
+  // Step 2: create a state variable
+  const [text, setText] = useState('Hello World')
+  // Step 10: create another state variable
+  const [boxText, setBoxText] = useState('')
+
+  function handleClick() {
+    // Step 3: update the state variable
+    setText('User has clicked the button')
   }
+
+  // Step 6: add useEffect
+  useEffect(() => {
+    // Step 7: add code that runs when the component mounts
+    console.log('Component has mounted')
+    setBoxText("boxes")
+    // Step 8: add code that runs when the component unmounts
+    return () => {
+      console.log('Component has unmounted')
+    }
+  }, [])
+
+  // Step 9: add code that runs when the state variable changes
+  useEffect(() => {
+    console.log('Text has changed')
+    // Step 11: update the state variable
+
+    if (text === 'Hello World') {
+      setBoxText("boxes")
+    } else {
+      setBoxText("boxes clicked")
+    }
+  }, [text])
+
   return (
     <>
-    {/* Step 1: change this to red */}
       <h1 className="header-text">React App</h1>
-      <p>Blank Template</p>
-      {/* step 2: create a flex box */}
+      {/* Step 4: add text, say this is helpful since you can just refer directly to the variable */}
+      <p>{text}</p>
       <div className='flex-container'>
-        <div>Box 1</div>
-        <div>Box 2</div>
-        <div>Box 3</div>
+        {/* Step 10: showing changes using useEffect on a state */}
+        <div>{boxText}</div>
+        <div>{boxText}</div>
+        <div>{boxText}</div>
       </div>
-      {/* Step 3: change color */}
-      <button onClick={changeColor}>Click Me</button>
+      <button onClick={handleClick}>Click Me</button>
     </>
   )
 }
