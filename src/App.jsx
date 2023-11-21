594c429e0e58db66b5982746f7f8666472dc1874
npham49 <brian.d.pham@gov.bc.ca>
2023-11-21 15:34:26 -0800

    added commented out code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
-import './App.css'
+// import './App.css'
+// import {useState,useEffect} from 'react'
 
 function App() {
-  // Step 3: create a function that chage the color of the header
-  const changeColor = () => {
-    const header = document.querySelector('.header-text')
-    header.style.color = 'purple'
-  }
+  // const [count, setCount] = useState(0);
+
+  // const handleClick = () => {
+  //   const header = document.querySelector('.header-text')
+  //   header.style.color = 'purple'
+  // }
+
+  // const handleClick = () => {
+  //   setCount(count + 1);
+  // };
+
+  // useEffect(() => {
+  //   console.log('useEffect')
+  //   const header = document.querySelector('.header-text')
+  //   header.style.color = 'green'
+  // }, [])
   return (
     <>
-    {/* Step 1: change this to red */}
       <h1 className="header-text">React App</h1>
       <p>Blank Template</p>
-      {/* step 2: create a flex box */}
-      <div className='flex-container'>
+      {/* <p>{count}</p> */}
+      {/* <p>
+        {count > 6 ? (
+          <div className="flex-container">
+            <div>Box 1</div>
+            <div>Box 2</div>
+            <div>Box 3</div>
+          </div>
+        ) : (
+          <p>Count lesser than 6</p>
+        )}
+      </p> */}
+      <div className="flex-container">
         <div>Box 1</div>
         <div>Box 2</div>
         <div>Box 3</div>
       </div>
-      {/* Step 3: change color */}
-      <button onClick={changeColor}>Click Me</button>
+      {/* <button onClick={handleClick}>Click Me</button> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
